ef1cd333ce05b62557d566dcb155e750147fb99c
William <41237516+williamrim@users.noreply.github.com>
2018-09-19 22:01:31 -0300

    aula 11

---
+++ Curso_Java_POO/src/s02/a11/course/Program.java
@@ -0,0 +1,35 @@
+package s02.a11.course;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		byte n1 = 126;
+		boolean complete = false;
+		char gender = 'F';
+		char letter = '\u0041';
+		int n2 = 1000;
+		int n3 = 2147483647;
+		long n4 = 21474836478L;
+		float n5 = 4.5f;
+		double n6 = 4.5;
+		String name = "Maria Green";
+		Object obj1 = "Alex Brown";
+		Object obj2 = 4.5f;
+
+		System.out.println(complete);
+		System.out.println(gender);
+		System.out.println(letter);
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(n4);
+		System.out.println(n4);
+		System.out.println(n4);
+		System.out.println(name);
+		System.out.println(obj1);
+		System.out.println(obj2);
+
+	}
+
+}
