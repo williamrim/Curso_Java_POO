42f2c15e3a48610908585bd0f049640d269ab013
William <41237516+williamrim@users.noreply.github.com>
2018-09-20 20:04:44 -0300

    aula 13

---
+++ Curso_Java_POO/src/s02/a13/course/Program.java
@@ -0,0 +1,45 @@
+package s02.a13.course;
+
+public class Program {
+
+	public static void main(String[] args) {
+	/*
+		double a;
+		float b;
+		a = 5.0;
+		b = (float) a;
+		System.out.println(b);
+	*/
+		
+	/*
+		double a;
+		int b;
+		a = 5.7;
+		b = (int) a;
+		System.out.println(b);
+	*/
+		
+	/*	
+		int a, b;
+		double result;
+		a = 5;
+		b = 2;
+		result = (double) a / b;
+		System.out.println(result);
+	*/
+		
+		
+		int a, b;
+		double result;
+		String s = "33";
+		
+		a = Integer.parseInt(s);
+		
+		result = Double.parseDouble(s);
+		
+		System.out.println(a);
+		System.out.println(result);
+		
+	}
+
+}
