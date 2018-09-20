5f859208323ced3fad725aab1cdba46ef92160ed
William <41237516+williamrim@users.noreply.github.com>
2018-09-20 20:07:27 -0300

    aula 11

--- Curso_Java_POO/src/s02/a11/course/Program.java
+++ Curso_Java_POO/src/s02/a11/course/Program.java
@@ -24,8 +24,8 @@ public class Program {
 		System.out.println(n2);
 		System.out.println(n3);
 		System.out.println(n4);
-		System.out.println(n4);
-		System.out.println(n4);
+		System.out.println(n5);
+		System.out.println(n6);
 		System.out.println(name);
 		System.out.println(obj1);
 		System.out.println(obj2);
