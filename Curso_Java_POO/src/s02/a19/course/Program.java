6d3fe6053c77192dce88f96980c4a8943d327698
wil <wil@wil-home>
2018-09-26 19:55:12 -0300

    aula 19

---
+++ Curso_Java_POO/src/s02/a19/course/Program.java
@@ -0,0 +1,16 @@
+package s02.a19.course;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		int n1 = 89;
+		int n2 = 60;
+
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+		System.out.println(n1 ^ n2);
+
+	}
+
+}
