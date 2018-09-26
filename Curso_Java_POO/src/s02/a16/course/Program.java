4f8d899844e0fcfc8a25550949790559f4929d2d
wil <wil@wil-home>
2018-09-26 19:12:13 -0300

    aula 16

---
+++ Curso_Java_POO/src/s02/a16/course/Program.java
@@ -0,0 +1,26 @@
+package s02.a16.course;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		double n1 = 10;
+		double n2 = 30;
+		String s = "ABC";
+
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(s);
+
+		System.out.println("----------------");
+
+		n1 *= 2; // 20
+		n2 += n1; // 50
+		s += "DEF";
+
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(s);
+	}
+
+}
---
+++ Curso_Java_POO/src/s02/a16/course/Program02.java
@@ -0,0 +1,22 @@
+package s02.a16.course;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		System.out.printf("a = %d%n", a);
+		int b = a++; //incrementa "a" depois de incrementar "b"
+		System.out.printf("a = %d%n", a);
+		System.out.printf("b = %d%n", b);
+		
+		int c = 100;
+		System.out.printf("c = %d%n", c);
+		int d = ++c; //incrementa "c" antes de incrementar "d"
+		System.out.printf("c = %d%n", c);
+		System.out.printf("d = %d%n", d);
+		
+		
+	}
+
+}
