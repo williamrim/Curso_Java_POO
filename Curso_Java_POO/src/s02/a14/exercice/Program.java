36bf751c51c47124937c7062f44773d742a3735a
wil <wil@wil-home>
2018-09-26 15:32:55 -0300

    exercício aula 14

--- Curso_Java_POO/src/s02/a14/exercice/Program.java
+++ Curso_Java_POO/src/s02/a14/exercice/Program.java
@@ -23,9 +23,9 @@ public class Program {
 		System.out.printf("Record: %d years old, code %d and gender: %c%n%n", age, code, gender);
 		System.out.printf("Measure with eight decimal places: %.8f%n", measure);
 		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
-		System.out.printf("BR decimal comma: %.3f%n", measure);
+		System.out.printf("US decimal comma: %.3f%n", measure);
 		Locale.setDefault(Locale.GERMAN);
-		System.out.printf("US decimal point: %.3f", measure);
+		System.out.printf("BR decimal point: %.3f", measure);
 	}
 
 }
