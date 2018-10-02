a55d65c98154c17e563fd2a9cb31ed425f475ff9
willimarim <williamrim@gmail.com>
2018-10-02 19:38:07 -0300

    aula 30

---
+++ Curso_Java_POO/src/s02/a30/course/Program.java
@@ -0,0 +1,25 @@
+package s02.a30.course;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a number: ");
+		double n = sc.nextDouble();
+
+		while (n >= 0.00) {
+			double sr = Math.sqrt(n);
+			System.out.printf("%.3f%n", sr);
+			System.out.println("Enter another number: ");
+			n = sc.nextDouble();
+		}
+		System.out.println("Negative number.");
+
+		sc.close();
+	}
+
+}
