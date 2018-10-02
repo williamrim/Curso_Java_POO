fef491509da96114c7355990da972fb71458ae59
willimarim <williamrim@gmail.com>
2018-10-01 21:11:54 -0300

    aula 25

---
+++ Curso_Java_POO/src/s02/a25/exercice/Program.java
@@ -0,0 +1,20 @@
+package s02.a25.exercice;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		double preco = 34.5;
+
+		/*
+		 * double desconto; if (preco < 20.0) { desconto = preco * 0.1; } else {
+		 * desconto = preco * 0.05; }
+		 */
+
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+		System.out.println(desconto);
+
+	}
+
+}
