84e3e1366f8a6c7644b0543abdf1f97d137818a7
willimarim <williamrim@gmail.com>
2018-10-01 21:17:15 -0300

    aula 26

---
+++ Curso_Java_POO/src/s02/a26/exercice/Program.java
@@ -0,0 +1,21 @@
+package s02.a26.exercice;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		double price = 400.00;
+
+		double discount;
+
+		if (price < 200.00) {
+			discount = price * 0.1;
+		} else {
+			discount = 0;
+		}
+
+		System.out.println(discount);
+
+	}
+
+}
