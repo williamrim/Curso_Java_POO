b7f59faf3bdbc324f80768224a35dbf5ee923bcb
willimarim <williamrim@gmail.com>
2018-10-15 20:34:53 -0300

    Sobrecarga

---
+++ Curso_Java_POO/src/s04/a58/application/Program.java
@@ -0,0 +1,43 @@
+package s04.a58.application;
+
+// Sobrecarga
+
+import java.util.Scanner;
+
+import s04.a58.entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		//Product p = new Product();
+		
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		Product product = new Product(name, price);
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
+		sc.close();
+
+	}
+
+}
