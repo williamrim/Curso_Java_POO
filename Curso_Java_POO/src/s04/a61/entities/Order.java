47eb319eff79de8dd1c0f14dd7f136acffdf6486
willimarim <williamrim@gmail.com>
2018-10-15 20:36:19 -0300

    Modificadores de acesso

---
+++ Curso_Java_POO/src/s04/a61/entities/Order.java
@@ -0,0 +1,29 @@
+package s04.a61.entities;
+
+//Modificadores de acesso
+
+import java.util.Date;
+
+public class Order {
+	
+	private Date date;
+	private Product product;
+	public Order(Date date, Product product) {
+		super();
+		this.date = date;
+		this.product = product;
+		this.product.name = "TV";
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
