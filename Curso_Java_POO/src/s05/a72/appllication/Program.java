f60d9f5f12d500f603a4103de761794d982ae375
williamrim <williamrim@gmail.com>
2018-10-21 10:58:05 -0300

    Boxing, unboxing e wrapper classes

---
+++ Curso_Java_POO/src/s05/a72/appllication/Program.java
@@ -0,0 +1,20 @@
+package s05.a72.appllication;
+
+// Boxing, unboxing e wrapper classes
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int x = 20;
+		
+		Integer obj = x;
+		
+		System.out.println(obj);
+		
+		int y = obj * 2;
+
+		System.out.println(y);
+	}
+
+}
