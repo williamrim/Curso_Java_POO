94d198d8cb298e8cda22678718a92d5f4328376c
williamrim <williamrim@gmail.com>
2018-11-20 12:47:12 -0200

    Exercício resolvido 2 (demo StringBuilder)

---
+++ Curso_Java_POO/src/s08/a99/entities/Comment.java
@@ -0,0 +1,24 @@
+package s08.a99.entities;
+
+//Exercício resolvido 2 (demo StringBuilder)
+
+public class Comment {
+	
+	private String Text;
+	
+	public Comment() {
+	}
+
+	public Comment(String text) {
+		Text = text;
+	}
+
+	public String getText() {
+		return Text;
+	}
+
+	public void setText(String text) {
+		Text = text;
+	}
+
+}
---
+++ Curso_Java_POO/src/s08/a99/entities/Post.java
@@ -0,0 +1,89 @@
+package s08.a99.entities;
+
+//Exercício resolvido 2 (demo StringBuilder)
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	private Date moment;
+	private String title;
+	private String content;
+	private Integer Likes;
+	
+	private List<Comment> comments = new ArrayList<>();
+	
+	public Post() {
+	}
+
+	public Post(Date moment, String title, String content, Integer likes) {
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		Likes = likes;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLikes() {
+		return Likes;
+	}
+
+	public void setLikes(Integer likes) {
+		Likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void addComment(Comment comment) {
+		comments.add(comment);
+	}
+	
+	public void removeComment(Comment comment) {
+		comments.remove(comment);
+	}
+	
+	public String toString() {
+		StringBuilder sd = new StringBuilder();
+		sd.append(title + "\n");
+		sd.append(Likes);
+		sd.append("Likes - ");
+		sd.append(sdf.format(moment) + "\n");
+		sd.append(content + "\n");
+		sd.append("Coments:\n");
+		for(Comment c: comments) {
+			sd.append(c.getText() + "\n");
+		}
+		return sd.toString();
+	}
+	
+}
