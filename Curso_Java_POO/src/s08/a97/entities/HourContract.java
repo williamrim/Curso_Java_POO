0a4a2d62e4bf7b1123b860eeeff5cf4e4d1460cb
williamrim <williamrim@gmail.com>
2018-11-20 11:52:11 -0200

    Exercício resolvido 1 - Parte 1

--- Curso_Java_POO/src/s08/a97/entities/HourContract.java
+++ Curso_Java_POO/src/s08/a97/entities/HourContract.java
@@ -43,4 +43,8 @@ public class HourContract {
 		this.hours = hours;
 	}
 	
+	public double totalValue() {
+		return valuePerHour * hours;
+	}
+	
 }
--- Curso_Java_POO/src/s08/a97/entities/Worker.java
+++ Curso_Java_POO/src/s08/a97/entities/Worker.java
@@ -3,6 +3,7 @@ package s08.a97.entities;
 //Exercício resolvido 1 - Parte 1
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import s08.a97.entities.enums.WorkerLevel;
@@ -62,10 +63,27 @@ public class Worker {
 		return contracts;
 	}
 
-	public void setContracts(List<HourContract> contracts) {
-		this.contracts = contracts;
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
 	}
 	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if(year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
 	
 	
 }
