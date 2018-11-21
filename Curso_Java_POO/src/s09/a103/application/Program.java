734f7c2bffe7d3fc7c7ce1fbc6eb0c67b129d686
williamrim <williamrim@gmail.com>
2018-11-21 19:27:42 -0200

    Herança

---
+++ Curso_Java_POO/src/s09/a103/application/Program.java
@@ -0,0 +1,19 @@
+package s09.a103.application;
+
+//Herança
+
+import s09.a103.entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		BusinessAccount account = new BusinessAccount();
+		
+		account.getBalance();
+				
+		
+
+	}
+
+}
