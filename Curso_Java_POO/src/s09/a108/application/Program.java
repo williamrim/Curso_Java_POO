851bfb8a2484de0e6bdc95c437481eac91503c99
williamrim <williamrim@gmail.com>
2018-11-27 09:41:25 -0200

    Exercício Resolvido

---
+++ Curso_Java_POO/src/s09/a108/application/Program.java
@@ -0,0 +1,54 @@
+package s09.a108.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import s09.a108.entities.Employee;
+import s09.a108.entities.OutsourcedEmployee;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		List<Employee> list = new ArrayList<>();
+
+		System.out.print("Enter the number of employees: ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Employee #" + i + " data:");
+			System.out.print("Outsourced (y/n)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Hours: ");
+			int hours = sc.nextInt();
+			System.out.print("Value per hours: ");
+			double valuePerHour = sc.nextDouble();
+			if (ch == 'y') {
+				System.out.print("Additional charge: ");
+				double additionalCharge = sc.nextDouble();
+				list.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+			}
+			else {
+				list.add(new Employee(name, hours, valuePerHour));
+			}
+			
+		}
+		
+		System.out.println("\nPAYMENTS:");
+		for(Employee emp : list) {
+			System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
+		}
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
