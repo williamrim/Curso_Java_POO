12b8f9af6c538ea6e7e3ee192c1928c21754108b
williamrim <williamrim@gmail.com>
2018-12-03 21:28:24 -0200

    Criando projeto e repositório git

---
+++ Curso_Java_POO/src/s11/chess/application/Program.java
@@ -0,0 +1,13 @@
+package s11.chess.application;
+
+//a126 - Criando projeto e repositório git
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+
+}
