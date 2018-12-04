7641d3767f71331ea0b96558cd7d0772ef7ac8dd
williamrim <williamrim@gmail.com>
2018-12-04 19:49:14 -0200

    ChessException e ChessPosition

---
+++ Curso_Java_POO/src/s11/chess/chess/ChessException.java
@@ -0,0 +1,9 @@
+package s11.chess.chess;
+
+public class ChessException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String msg) {
+		super(msg);
+	}
+}
